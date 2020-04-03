022ae08dad906723f559474780ba5c4a3fff7833
Igor Van-Dúnem <vandunem.igor@gmail.com>
2020-04-03 14:38:47 +0100

    Setup the app for Heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
 const app = express();
+const port = process.env.PORT || 3000;
+
 // Define paths for Express config
 const directoryPath = path.join(__dirname, "../public");
 const viewPath = path.join(__dirname, "../templates/views");
@@ -94,6 +96,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server Up => Port 3000");
+app.listen(port, () => {
+  console.log("Server Up => Port " + port);
 });
